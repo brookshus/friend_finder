4892789a4ea67385b79c13f2ac1b245935a147d7
stbrooks <stephanie.brooks@outlook.com>
2019-07-11 19:12:18 -0700

    updated

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsList = [
+	{
+	  name: "Martha",
+	  photo: "https://www.clackamas.us/sites/default/files/2019-02/mschrader_sm_0.jpg",
+	  preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+	},
+
+	{
+	  name: "Kraig",
+	  photo: "https://ed.psu.edu/directory/czc270/@@images/photo.jpeg",
+	  preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
+	},
+
+	{
+	  name: "Anthony",
+	  photo: "https://i0.wp.com/catholicsaints.info/wp-content/uploads/img-Saint-Anthony-of-Padua1.jpg?resize=299%2C400",
+	  preferences: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+	},
+];
+
+module.exports = friendsList;
